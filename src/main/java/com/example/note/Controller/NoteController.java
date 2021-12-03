fc774e623c260ea87deee4b1b36c2b9e77405ff1
phuoc1707 <tdphuoc77@gmail.com>
2021-12-03 10:22:37 +0700

    update cors

--- src/main/java/com/example/note/Controller/NoteController.java
+++ src/main/java/com/example/note/Controller/NoteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/notes")
 public class NoteController {
--- src/main/java/com/example/note/config/SercurityConfig.java
+++ src/main/java/com/example/note/config/SercurityConfig.java
@@ -2,17 +2,26 @@ package com.example.note.config;
 
 import com.example.note.filter.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+//@Configuration
+//@EnableWebMvc
 public class SercurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
